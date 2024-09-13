3e96294c454d5f139d455d79ca8fe505d8f3c21a
harsh4677 <harshsantoshsalve@gmail.com>
2024-09-13 09:01:00 +0530

    solved Left rotate an array by one palce

--- Array/Easy/LeftRotateByOneplace.cpp
+++ Array/Easy/LeftRotateByOneplace.cpp
@@ -1,11 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> leftRotate(vector<int>& arr){
-    
+
+vector<int> ROotateArray(vector<int>& arr){
+    int n = arr.size();
+
+    int temp = arr[0];
+    for(int i=0; i<n; i++){
+        arr[i] = arr[i+1];
+    }
+    arr[n-1] = temp;
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
-
-int main(){
-
-}
-
